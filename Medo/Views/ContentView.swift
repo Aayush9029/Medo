eac7be3ac14330a27ad1c2cb66fff1b5f66a043b
Aayush9029 <aayushpokharel36@gmail.com>
2022-12-26 00:49:22 -0500

    test

--- Medo/Views/ContentView.swift
+++ Medo/Views/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
         }
         .alert(isPresented: $taskViewModel.confrimDelete) {
             Alert(
-                title: Text("Remove all Tasks?"),
+                title: Text("Remove all tasks?"),
                 message: Text("This will permanently remove all tasks."),
                 primaryButton: .cancel(),
                 secondaryButton: .destructive(Text("Remove all"), action: deleteAll)
